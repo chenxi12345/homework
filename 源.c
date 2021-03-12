154e9787b784216185b4ba0af342b309ef043dbd
chenxi12345 <73779067+chenxi12345@users.noreply.github.com>
2021-03-12 21:40:35 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
+void Hanoi(int, char, char, char);
+int n = 0;
 int main(void)
 {
 	int n;
-	printf("请输入一个年份：");
+	printf("请输入盘子个数：");
 	scanf_s("%d", &n);
-	printf("\n");
-	if ((n % 4 == 0) && (n % 100 != 0) || (n % 400 == 0))
-		printf("%d年是闰年", n);
+	printf("==========================\n");
+	Hanoi(n, 'A', 'B', 'C');
+	printf("==========================\n");
+}
+void Hanoi(int n, char a, char b, char c)
+{
+	if (n == 1)
+		printf("step%3d:%c-->%c\n", n++, a, c);
 	else
-		printf("%d年不是闰年", n);
+	{
+		Hanoi(n - 1, a, c, b);
+		printf("step%3d:%c-->%c\n", n++, a, c);
+		Hanoi(n - 1, b, a, c);
+	}
 }
